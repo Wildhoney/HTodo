ff32f36053a036d85412867cbec41288d93d38fb
Wildhoney <adam.timberlake@gmail.com>
2016-07-19 14:24:52 +0100

    Improved removeTodo

--- htodo/app/Main.hs
+++ htodo/app/Main.hs
@@ -1,13 +1,10 @@
 module Main where
 
-import Control.Monad.IO.Class (liftIO)
-
-import Store (addTodo)
+import Store       (addTodo, removeTodo)
 import Interactive (promptAdd)
 
 main :: IO ()
 main = do
-    task  <- promptAdd
-    todos <- return $ addTodo task []
+    task     <- promptAdd
+    let todos = addTodo task []
     putStrLn $ show $ todos
-    return ()
--- htodo/src/Store.hs
+++ htodo/src/Store.hs
@@ -1,10 +1,14 @@
-module Store (addTodo) where
+module Store (addTodo, removeTodo) where
 
-import Data.List (delete)
+import Data.List   (delete)
+import Data.Either (Either(..))
 
 addTodo :: Maybe a -> [a] -> [a]
 addTodo (Just task) tasks = task:tasks
 addTodo Nothing tasks     = tasks
 
-removeTodo :: Eq a => Int -> [a] -> [a]
-removeTodo index tasks    = delete (tasks !! index) tasks
+removeTodo :: Eq a => Int -> [a] -> Either String [a]
+removeTodo index tasks | inRange   = Right $ delete (tasks !! index) tasks
+                       | otherwise = Left "Index too large"
+    where
+        inRange = index < length tasks
