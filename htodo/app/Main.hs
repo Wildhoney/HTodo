b8b4e40d4f0dbf424cc9308073cc41731ddcc97c
Wildhoney <adam.timberlake@gmail.com>
2016-08-20 22:44:13 +0100

    Added title fn

--- htodo/app/Main.hs
+++ htodo/app/Main.hs
@@ -3,20 +3,23 @@ module Main where
 import System.Environment  (getArgs)
 import System.Exit         (exitWith, ExitCode(..))
 import Store               (addTodo, removeTodo, enumTodos)
-import System.Console.ANSI (setSGR, SGR(SetColor, Reset), ConsoleLayer(Foreground), ColorIntensity(Vivid), Color(Cyan))
+import System.Console.ANSI (setSGR, SGR(SetColor, SetConsoleIntensity, Reset), ConsoleIntensity(..), ConsoleLayer(Foreground),
+                            ColorIntensity(Vivid), Color(Cyan))
 
 main = getArgs >>= parse
 
+title = setSGR [SetColor Foreground Vivid Cyan] >>
+        setSGR [SetConsoleIntensity BoldIntensity] >>
+        putStrLn "\nHTodo\n" >>
+        setSGR [Reset]
+
 parse :: [String] -> IO ()
-parse ["+", todo]  = addTodo todo
-parse ["-", index] = removeTodo (read index :: Integer)
-parse []           = enumTodos >>= enumerate >> exit
+parse ["+", todo]  = title >> addTodo todo
+parse ["-", index] = title >> removeTodo (read index :: Integer)
+parse []           = title >> enumTodos >>= enumerate >> exit
     where
-        transform = \todo -> (show (fst todo) :: String) ++ ": " ++ snd todo
-        enumerate = \todos ->
-                        setSGR [SetColor Foreground Vivid Cyan] >>
-                        (putStrLn . unlines . map transform) todos >>
-                        setSGR [Reset]
+        transform = \todo -> " " ++ (show (fst todo) :: String) ++ ": " ++ snd todo
+        enumerate = putStrLn . unlines . map transform
 
 usage   = putStrLn "Usage: htodo -/+ [task/index ..]"
 version = putStrLn "Haskell htodo 0.1"
